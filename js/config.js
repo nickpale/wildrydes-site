c47a70be1e88e5ef552f0b780ee9347c5e7b3f20
Nick White <nick@nickpale.com>
2020-10-18 08:43:21 -0700

    Add AWS Cognito configs

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_ckycNYCV6',
+        userPoolClientId: '72efh3oq9bu0r8hoi5g2655l6j',
+        region: 'us-east-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
